79cd0e226ead5e060315cbf6c6f3d1c4179f5bd6
Dell-User <Dell-User@DESKTOP-88R8045>
2023-09-10 13:49:41 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCharecters = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(529, 132)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(85, 33)
+        Me.btnSubmit.TabIndex = 0
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 92)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(118, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter Numerics"
+        '
+        'txtCharecters
+        '
+        Me.txtCharecters.Location = New System.Drawing.Point(529, 89)
+        Me.txtCharecters.Name = "txtCharecters"
+        Me.txtCharecters.Size = New System.Drawing.Size(100, 26)
+        Me.txtCharecters.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtCharecters)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtCharecters As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub btnButton_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        'Try Catch
+        Dim isNumber As String
+        isNumber = txtCharecters.Text
+
+
+        Try
+            'If entered a numeric number
+            Dim numericValue As Double
+            numericValue = (isNumber)
+
+            'If entered a numeric number display the results
+            MsgBox("Valid: " & numericValue & " Is A Number")
+
+        Catch ex As Exception
+            'If entered a non-numeric number display results
+            MsgBox("Invalid: " & isNumber & " Is Not a number")
+        End Try
+
+    End Sub
+End Class
+
+
